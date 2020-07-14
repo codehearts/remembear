fcb7103a96e03ac603bfc462e36209658b9beee6
Kate Hart <codehearts@users.noreply.github.com>
2020-07-13 21:44:44 -0700

    Silence Diesel migration output during testing

--- tests/common_database/mod.rs
+++ tests/common_database/mod.rs
@@ -2,13 +2,19 @@
 
 use super::common::Result;
 use remembear::database::{self, Database};
+use std::io::sink;
 use std::sync::Arc;
 
 /// Creates a connection to a unique temporary in-memory sqlite3 database
 #[inline]
 pub fn new() -> Result<Arc<dyn Database>> {
     let database = database::Sqlite::connect(":memory:")?;
-    diesel_migrations::run_pending_migrations(database.connection())?;
+
+    diesel_migrations::run_pending_migrations_in_directory(
+        database.connection(),
+        &diesel_migrations::find_migrations_directory()?,
+        &mut sink(),
+    )?;
 
     Ok(Arc::new(database))
 }

368e5c9815a143bab885a3dcccdfcdff4d4a6828
Kate Hart <codehearts@users.noreply.github.com>
2020-08-15 03:32:44 -0700

    Refactor dependency initialization

--- src/lib.rs
+++ src/lib.rs
@@ -16,13 +16,13 @@ pub mod schedule;
 pub mod scheduler;
 pub mod user;
 
+pub use crate::config::Config;
 pub use command::execute;
 pub use reminder::model::Reminder;
 pub use schedule::model::Schedule;
 pub use scheduler::model::Scheduler;
 pub use user::model::User;
 
-use crate::config::Config;
 use database::Database;
 use std::error::Error;
 use std::sync::Arc;
@@ -33,14 +33,24 @@ pub struct Dependencies {
     pub database: Arc<dyn database::Database>,
 }
 
-/// Initializes and configures all dependencies
-///
-/// # Errors
-///
-/// When there is an error with the config or any of the dependencies
-pub fn initialize_dependencies() -> Result<Dependencies, Box<dyn Error>> {
-    let config = Config::load("remembear")?;
-    let database = Arc::new(database::Sqlite::connect(&config.database.sqlite.path)?);
+impl Dependencies {
+    /// Initializes and configures all dependencies
+    ///
+    /// # Errors
+    ///
+    /// When there is an error with the config or any of the dependencies
+    pub fn new(config: &Config) -> Result<Self, Box<dyn Error>> {
+        let database = Arc::new(database::Sqlite::connect(&config.database.sqlite.path)?);
 
-    Ok(Dependencies { database })
+        Ok(Self { database })
+    }
+}
+}
+
+/// Providers for service data
+pub struct Providers<'a> {
+    /// Provider for user data
+    pub user: &'a dyn crate::user::provider::Providable,
+    /// Provider for reminder data
+    pub reminder: &'a dyn crate::reminder::provider::Providable,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use remembear::command::{self, execute};
-use remembear::{initialize_dependencies, reminder, user};
+use remembear::{reminder, user, Config, Dependencies, Providers};
 use std::error::Error;
 use std::sync::Arc;
 use structopt::StructOpt;
@@ -7,12 +7,14 @@ use structopt::StructOpt;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let command = command::Global::from_args();
-    let dependencies = initialize_dependencies()?;
+
+    let config = Config::load("remembear")?;
+    let dependencies = Dependencies::new(&config)?;
 
     let user_provider = user::Provider::new(Arc::clone(&dependencies.database));
     let reminder_provider = reminder::Provider::new(Arc::clone(&dependencies.database));
 
-    let providers = command::Providers {
+    let providers = Providers {
         user: &user_provider,
         reminder: &reminder_provider,
     };
